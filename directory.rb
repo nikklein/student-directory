c6fadc92906a3e96fd17967598ecc522477b5e00
nikklein <objcoding@gmail.com>
2016-06-22 22:41:34 +0300

    Exercise 6: Add the method center

--- directory.rb
+++ directory.rb
@@ -1,11 +1,11 @@
 def input_students
   puts "Please enter the names of the students\nTo finish, just hit return twice"
-
+# Country of Birth == COB
 students = [
-{name: "Dr. Hannibal Lecter", cohort: :november, hobby: "Humans eating",  origin: 'USA', height: 1.80,t_shirt_size: 'M'},
-{name: "Darth Vader", cohort: :november, hobby: "Piloting a TIE", origin: 'Galaxy', height: 1.85,t_shirt_size: 'L'},
-{name: "Nurse Ratched",cohort: :november, hobby: "Acting like a tyrant", origin: 'USA', height: 1.65,t_shirt_size: 'M'},
-{name: "Michael Corleone",cohort: :november, hobby: "Traveling to Sicily", origin: 'USA', height: 1.75,t_shirt_size: 'S'},
+{name: "Dr. Hannibal Lecter", cohort: :november, hobby: "Humans eating",  COB: 'USA', height: 1.80,t_shirt_size: 'M'},
+{name: "Darth Vader", cohort: :november, hobby: "Piloting a TIE", COB: 'Galaxy', height: 1.85,t_shirt_size: 'L'},
+{name: "Nurse Ratched",cohort: :november, hobby: "Acting like a tyrant", COB: 'USA', height: 1.65,t_shirt_size: 'M'},
+{name: "Michael Corleone",cohort: :november, hobby: "Traveling to Sicily", COB: 'USA', height: 1.75,t_shirt_size: 'S'},
   ]
 
 name = gets.chomp
@@ -19,14 +19,15 @@ while !name.empty? do
   end
 
 def print_header
- puts "The students of Villains Academy"
- puts "-------------"
+ puts "The students of Villains Academy".center(50)
+ puts "-------------".center(50)
 end
 
 def print students
 count = 0
 while count < students.size do
- puts "#{students[count][:name]} |#{students[count][:cohort]} cohort|hobby: #{students[count][:hobby]}|origin: #{students[count][:origin]}|height: #{students[count][:height]}|T-shirt size:#{students[count][:t_shirt_size]}|"
+ puts "#{students[count][:name]} (#{students[count][:cohort]} cohort) hobby: #{students[count][:hobby]}
+country of birth: #{students[count][:COB]}  height: #{students[count][:height]} T-shirt size: #{students[count][:t_shirt_size]}"
 count += 1
 end
 end
