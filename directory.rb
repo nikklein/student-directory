5e92e6ff0a3feeed7b0aceaf339bc18060d56d86
nikklein <objcoding@gmail.com>
2016-06-25 23:00:58 +0300

    Add interactive menu

--- directory.rb
+++ directory.rb
@@ -1,16 +1,29 @@
+def interactive_menu
+students = []
+loop do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+
+  selection = gets.chomp
+
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    print_header students
+    print students
+    print_footer students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students
 puts "Please enter the name of a student and a cohort.\nTo finish, just hit return twice."
-
-# Country of Birth == COB
-
-=begin
-students = [
-{name: "Dr.Hannibal Lecter", cohort: :november, hobby: "Humans eating",  COB: 'USA', height: '1.80',t_shirt_size: 'M'},
-{name: "Darth Vader", cohort: :december, hobby: "Piloting a TIE", COB: 'Galaxy', height: '1.85',t_shirt_size: 'L'},
-{name: "Nurse Ratched",cohort: :januar, hobby: "Acting like a tyrant", COB: 'USA', height: '1.65',t_shirt_size: 'M'},
-{name: "Michael Corleone",cohort: :november, hobby: "Traveling to Sicily", COB: 'USA', height: '1.75',t_shirt_size: 'S'},
-  ]
-=end
 
 students = []
 
@@ -28,7 +41,6 @@ puts "..and a cohort please"
 cohort = gets.chomp
   end
   students
-
   end
 
 
@@ -109,9 +121,10 @@ end
 
 end
 
-students = input_students
+interactive_menu
+#students = input_students
 print_header students
 #print_by_cohort students
 print students
 print_footer students
-typo students
+#typo students
