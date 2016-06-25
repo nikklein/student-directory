b1547abe77a7a4ada3e4f5abe8713d6f6d556068
nikklein <objcoding@gmail.com>
2016-06-25 14:53:33 +0300

    Exercise 7: Add input for a cohort, provide default value if a cohort value is empty, convert input to a string, convert input to a symbol, a typo fixing method

--- directory.rb
+++ directory.rb
@@ -1,19 +1,25 @@
 def input_students
-  puts "Please enter the names of the students\nTo finish, just hit return twice"
+puts "Please enter the name of the student and a cohort.\nTo finish, just hit return twice."
 # Country of Birth == COB
 students = [
-{name: "Dr. Hannibal Lecter", cohort: :november, hobby: "Humans eating",  COB: 'USA', height: 1.80,t_shirt_size: 'M'},
-{name: "Darth Vader", cohort: :november, hobby: "Piloting a TIE", COB: 'Galaxy', height: 1.85,t_shirt_size: 'L'},
-{name: "Nurse Ratched",cohort: :november, hobby: "Acting like a tyrant", COB: 'USA', height: 1.65,t_shirt_size: 'M'},
-{name: "Michael Corleone",cohort: :november, hobby: "Traveling to Sicily", COB: 'USA', height: 1.75,t_shirt_size: 'S'},
+{name: "Dr. Hannibal Lecter", cohort: :november, hobby: "Humans eating",  COB: 'USA', height: '1.80',t_shirt_size: 'M'},
+{name: "Darth Vader", cohort: :november, hobby: "Piloting a TIE", COB: 'Galaxy', height: '1.85',t_shirt_size: 'L'},
+{name: "Nurse Ratched",cohort: :november, hobby: "Acting like a tyrant", COB: 'USA', height: '1.65',t_shirt_size: 'M'},
+{name: "Michael Corleone",cohort: :november, hobby: "Traveling to Sicily", COB: 'USA', height: '1.75',t_shirt_size: 'S'},
   ]
 
 name = gets.chomp
+puts "..and a cohort please"
+cohort = gets.chomp
 
 while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students "
-    name = gets.chomp
+  cohort = :november if cohort.empty?
+  students << {name: name, cohort: cohort.downcase.to_sym}
+  puts "Now we have #{students.count} students "
+  puts"Please enter student's name or return twice to exit"
+  name = gets.chomp
+puts "..and cohort please"
+cohort = gets.chomp
   end
   students
   end
@@ -26,17 +32,57 @@ end
 def print students
 count = 0
 while count < students.size do
- puts "#{students[count][:name]} (#{students[count][:cohort]} cohort) hobby: #{students[count][:hobby]}
+ puts "#{count + 1}. #{students[count][:name]} (#{students[count][:cohort]} cohort) hobby: #{students[count][:hobby]}
 country of birth: #{students[count][:COB]}  height: #{students[count][:height]} T-shirt size: #{students[count][:t_shirt_size]}"
 count += 1
 end
 end
 
 def print_footer names
+puts
 puts "Overall, we have #{names.count} great students"
 end
+
+def typo students
+puts
+puts "If you made a typo you could fix it now, to do that please enter 'yes', otherwise enter 'no':"
+overwrite = gets.chomp.downcase.to_s
+until overwrite == 'yes' or overwrite == 'no' do
+  puts "Plese enter 'yes' or 'no'"
+  overwrite = gets.chomp.downcase.to_s
+end
+
+exit if overwrite == "no"
+
+puts "Please enter a number from numbered list of the students above:"
+stud_number = gets.chomp.to_s
+until stud_number.to_i <= students.size and stud_number.to_i  > 0 do
+puts "Please enter a number not equal to '0' and less than #{students.size}: "
+  stud_number = gets.chomp.to_s
+end
+
+puts "Please enter one of the following: 'name', 'cohort', 'hobby', 'COB', 'height', 't_shirt_size'? "
+key_choose = gets.chomp.to_s.to_sym
+
+until students[stud_number.to_i - 1].key?(key_choose) or key_choose == :exit do
+puts "Please enter one of these words: 'name', 'cohort', 'hobby', 'COB', 'height', 't_shirt_size' or 'exit' to quit!"
+key_choose = gets.chomp.to_s.to_sym
+end
+exit if key_choose == :exit
+puts "Please enter correct info"
+change_info = gets.chomp.to_s
+students[stud_number.to_i - 1].map{|k,v| students[stud_number.to_i - 1][key_choose] = change_info.to_sym } if key_choose == :cohort
+students[stud_number.to_i - 1][key_choose].to_s.replace(change_info)
+print_header
+print students
+print_footer students
+puts
+typo students
+end
+
 
 students = input_students
 print_header
 print students
 print_footer students
+typo students
